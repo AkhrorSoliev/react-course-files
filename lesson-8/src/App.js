5273c5afd9352be04c88cccd71f50e7ef685369f
akhrorsoliev <akhrorteacher@gmai.com>
2023-03-19 20:34:05 +0500

    lesson-9 files

--- lesson-8/src/App.js
+++ lesson-8/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='./logo.svg' className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Akhror web
         </a>
       </header>
     </div>
