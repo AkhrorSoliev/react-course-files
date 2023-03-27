efbc9f3a94bfef19f8c4f205f40dc75dec4fed7d
akhrorsoliev <akhrorteacher@gmai.com>
2023-03-27 14:56:21 +0500

    lesson-15 files

--- lesson-14/src/App.js
+++ lesson-14/src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
 import { useState } from 'react'
+import Title from './components/Title';
 
 // useState 
 
 function App() {
-  const [name, setName] = useState("Ahror")
+  const [showConent, setShowContent] = useState(true)
   const [events, setEvents] = useState([
     {title: "akhror's birthday party", id: 1},
     {title: "doniyor's live stream", id: 2},
     {title: "match: manchester united vs barcelona", id: 3}
   ])
-  const [showEvents, setShowEvents] = useState(true)
   
-  const handleClick = () => {
-    setName('Doniyor')
-  }
-
-
   // delete items
   const handleDelete = (id) => {
     setEvents((prev) => {
@@ -30,25 +25,21 @@ function App() {
 
   return (
     <div className="App">
-        <h1>My name is {name}</h1>
-        <button onClick={handleClick}>Change name</button>
-
+        <Title/>
         <hr />
         <br />
-
-        {showEvents && <button onClick={() => {setShowEvents(false)}}>Hide Events</button>}
-        {!showEvents && <button onClick={() => {setShowEvents(true)}}>Show Events</button>}
-        {showEvents && <div>
-          {events.length === 0 && <div> <h5>No items left</h5> </div>}
-          {events.map((event) => {
-            return (
-              <div key={event.id}>
-                <h2>{event.title}</h2>
-                <button onClick={() => {handleDelete(event.id)}}>Delete</button>
-              </div>
-            )
-          })}
-        </div>}
+        {showConent && <button onClick={() => setShowContent(false)}>Hide Conent</button>}
+        {!showConent && <button onClick={() => setShowContent(true)}>Show Conent</button>}
+        {showConent && <div>
+          {events.length === 0 && <h3>Not Conent Yet :(</h3>}
+         {events.map((event) => {
+          return (
+            <div key={event.id}>
+              <h2>{event.title}</h2>
+              <button onClick={() => handleDelete(event.id)}>Delete</button>
+            </div>
+          )
+        })} </div>}
     </div>
   );
 }
