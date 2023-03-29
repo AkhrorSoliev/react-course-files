ab5c8d2e6d13af5ab74a870714b7025cb2afb20f
akhrorsoliev <akhrorteacher@gmai.com>
2023-03-29 15:57:14 +0500

    lesson-17 files

--- lesson-17/src/App.js
+++ lesson-17/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react'
 import Title from './components/Title'
+import { Fragment } from 'react';
 
 function App() {
   const [showConent, setShowContent] = useState(true)
@@ -19,20 +20,21 @@ function App() {
     }) 
   }
 
+  const subtitle = "All events well be here :)"
+
   return (
     <div className="App">
-        <Title title="Akhror's Kingdom👑 Events" subtitle="All events well be here :)"/>
-        <Title title="Doniyor's Kingdom👑 Events" subtitle="All events well be here :)"/>
+        <Title title="Akhror's Kingdom👑 Events" subtitle={subtitle}/>
         {showConent && <button onClick={() => setShowContent(false)}>Hide Conent</button>}
         {!showConent && <button onClick={() => setShowContent(true)}>Show Conent</button>}
         {showConent && <div>
           {events.length === 0 && <h3>Not Conent Yet :(</h3>}
          {events.map((event) => {
           return (
-            <div key={event.id}>
+            <Fragment key={event.id}>
               <h2>{event.title}</h2>
               <button onClick={() => handleDelete(event.id)}>Delete</button>
-            </div>
+            </Fragment>
           )
         })} </div>}
     </div>
--- lesson-17/src/components/Title.js
+++ lesson-17/src/components/Title.js
@@ -1,13 +1,12 @@
 
 
 const Title = ({title, subtitle}) => {
-  
     return (
-        <div>
+        <>
             <h1 className="title">{title}</h1>
             <br />
             <p className="subtitle">{subtitle}</p>
-        </div>
+        </>
     )
 }
 
