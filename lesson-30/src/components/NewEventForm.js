90a2e3bf5e7aa6a3db84264ddee8c74287163cf9
akhrorsoliev <akhrorteacher@gmai.com>
2023-04-15 09:50:16 +0500

    lesson-30 files

--- lesson-30/src/components/NewEventForm.js
+++ lesson-30/src/components/NewEventForm.js
@@ -7,22 +7,22 @@ function NewEventForm() {
   const [title, setTitle] = useState('')
   const [date, setDate] = useState('')
 
-  const reset = () => {
-    setDate('')
+  const resetInputs = () => {
     setTitle('')
+    setDate('')
   }
 
   return (
     <form className="new-event-form">
       <label>
         <span>Event Title:</span>
-        <input type="text" onChange={(e) => {setTitle(e.target.value)}} value={title}/>
+        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e) => {setDate(e.target.value)}} value={date}/>
+        <input type="date" onChange={(e) => setDate(e.target.value)} value={date}/>
       </label>
-      <button onClick={reset} type="button">reset form</button>
+      <button onClick={resetInputs} type="button">reset inputs</button>
       <br />
       <br />
       <button>Submit</button>
