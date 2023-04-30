0891cbee4176cfda8e5b33dec3b02842f550e625
akhrorsoliev <akhrorteacher@gmai.com>
2023-04-30 17:05:42 +0500

    lesson-37 files

--- lesson-37/src/App.jsx
+++ lesson-37/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import TripList from './components/TripList'
 
 function App() {
   return (
     <div className="App">
-     <h1>Trips ❤️</h1>
+      <TripList/>
     </div>
   )
 }
